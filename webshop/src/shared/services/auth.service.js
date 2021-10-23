f6edf8d5df523d8000b91df818226206ecc76439
tamazs <majortamas313@gmail.com>
2021-10-23 15:48:50 +0200

    signup and auth setup

---
+++ webshop/src/shared/services/auth.service.js
@@ -0,0 +1,21 @@
+import {auth} from "@/firebase";
+
+export function signUp(email, password) {
+    return auth.createUserWithEmailAndPassword(email, password)
+}
+
+export function login(email, password) {
+    return auth.signInWithEmailAndPassword(email, password)
+}
+
+export function logout() {
+    return auth.signOut()
+}
+
+export function getCurrentUser() {
+    return auth.currentUser
+}
+
+export function updateUser(data) {
+    return auth.updateCurrentUser(data)
+}
--- webshop/src/views/Signup.vue
+++ webshop/src/views/Signup.vue
@@ -1,8 +1,8 @@
 <template>
-<div id="signupbody">
+  <div id="signupbody">
     <form
         class="p-5"
-      @submit.prevent="register"
+        @submit.prevent="checkAndSave"
     >
       <div class="container">
         <div class="row justify-content-center">
@@ -12,64 +12,72 @@
                 <h3 class="font-weight-light mb-3 text-white text-uppercase">Sign up</h3>
                 <div class="form-row">
                   <div
-                    v-if="error"
-                    class="col-12 alert alert-danger px-3"
+                      v-if="errors.length"
+                      class="col-12 alert alert-danger px-3"
                   >
-                    {{ error }}
+                    <div v-for="error in errors" :key="error">
+                      {{ error }}
+                    </div>
                   </div>
-                  <section class="col-sm-8 form-group">
-                    <label
-                      class="form-control-label sr-only"
-                      for="displayName"
-                    >Display Name</label>
-                    <input
-                      class="form-control"
-                      type="text"
-                      id="displayName"
-                      placeholder="Display Name"
-                      name="displayName"
-                      required
-                      v-model="displayName"
-                    />
-                  </section>
                 </div>
                 <section class="col-sm-8 form-group mt-3">
                   <input
-                    class="form-control"
-                    type="email"
-                    id="email"
-                    placeholder="Email Address"
-                    required
-                    name="email"
-                    v-model="email"
+                      class="form-control"
+                      type="email"
+                      id="email"
+                      placeholder="Email Address"
+                      required
+                      name="email"
+                      v-model="email"
                   />
                 </section>
                 <div class="form-row">
                   <section class="col-sm-8 form-group mt-3">
                     <input
-                      class="form-control"
-                      type="password"
-                      placeholder="Password"
-                      v-model="passOne"
+                        class="form-control"
+                        type="password"
+                        placeholder="Password"
+                        v-model="password"
                     />
                   </section>
                   <section class="col-sm-8 form-group mt-3">
                     <input
-                      class="form-control"
-                      type="password"
-                      required
-                      placeholder="Repeat Password"
-                      v-model="passTwo"
+                        class="form-control"
+                        type="password"
+                        required
+                        placeholder="Repeat Password"
+                        v-model="passwordRepeat"
                     />
                   </section>
                 </div>
                 <div class="form-group text-right mb-0">
                   <button
-                    class="btn btn-outline-light p-2 mt-5"
-                    type="submit"
+                      class="btn btn-outline-light p-2 mt-5"
+                      type="submit"
                   >
                     Sign up
                   </button>
+                </div>
+                <div class="position-fixed top-0 end-2" style="z-index: 11">
+                  <div
+                      class="toast align-items-center"
+                      role="alert"
+                      aria-live="assertive"
+                      aria-atomic="true"
+                      id="successToast"
+                  >
+                    <div class="d-flex">
+                      <div class="toast-body">
+                        Your profile has been created successfully and you are logged in!
+                      </div>
+                      <button
+                          type="button"
+                          class="btn-close me-2 m-auto"
+                          data-bs-dismiss="toast"
+                          aria-label="Close"
+                      ></button>
+                    </div>
+                  </div>
                 </div>
               </div>
             </div>
@@ -85,8 +93,45 @@
 </template>
 
 <script>
+
+const authService = require("@/shared/services/auth.service");
+
 export default {
-   
+  data: () => ({
+    password: null,
+    passwordRepeat: null,
+    email: null,
+    errors: [],
+    role: 'admin'
+  }),
+  methods: {
+    checkAndSave() {
+      this.errors = [];
+
+      if (!this.password || !this.passwordRepeat) {
+        this.errors.push("Password required");
+      } else if (this.password !== this.passwordRepeat) {
+        this.errors.push("Password is not matching")
+      }
+
+      if (!this.email) {
+        this.errors.push('Email required.');
+      } else if (!this.validEmail(this.email)) {
+        this.errors.push('Valid email required.');
+      }
+
+      if (!this.errors.length) {
+        authService.signUp(this.email, this.password).then(() => {
+          this.$router.push({name:"Shop"})
+        })
+      }
+
+    },
+    validEmail(email) {
+      const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+      return re.test(email)
+    }
+  }
 }
 </script>
 
@@ -94,15 +139,15 @@ export default {
 @import url('https://fonts.googleapis.com/css2?family=Raleway:wght@100;400;700&display=swap');
 
 #signupbody {
-    height: 100vh;
-    width: 100vw;
-    background-color: black;
-    overflow: hidden;
-    font-family: 'Raleway', sans-serif;
+  height: 100vh;
+  width: 100vw;
+  background-color: black;
+  overflow: hidden;
+  font-family: 'Raleway', sans-serif;
 }
 
 form {
-    top: 30%;
+  top: 30%;
 }
 
 #card {
