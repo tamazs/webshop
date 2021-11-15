aeef2482c1bc97c7bf4cefa530d68e60805b9a0f
tamazs <majortamas313@gmail.com>
2021-11-15 15:31:02 +0100

    new modal package register

--- webshop/src/main.js
+++ webshop/src/main.js
@@ -1,14 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-import store from './store'
+import {store} from './store'
 import 'bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import CursorFx from '@luxdamore/vue-cursor-fx';
 import '@luxdamore/vue-cursor-fx/dist/CursorFx.css';
-
+import VModal from 'vue-js-modal'
 
 Vue.config.productionTip = false
+Vue.use(VModal, { componentName: 'Modal' })
 
 Vue.use(
   CursorFx
